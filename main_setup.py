661d63835f34ebd2c18126e5d96d37f19d5f68f9
Kerrington Jackson <kerryjac@umich.edu>
2025-12-14 03:00:58 -0500

    made adjustment to tables to avoid duplicate data

--- main_setup.py
+++ main_setup.py
@@ -4,14 +4,23 @@ def setup_database():
     conn = sqlite3.connect("project.db")
     cur = conn.cursor()
 
+    #cuisines table
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Cuisines (
+        id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE
+    );
+    """)
+
     # for recipies table
     cur.execute("""
     CREATE TABLE IF NOT EXISTS Recipes (
         id INTEGER PRIMARY KEY,
         recipe_id INTEGER UNIQUE,
         title TEXT,
-        cuisine TEXT,
+        cuisine_id INTEGER
         ready_time INTEGER
+        source TEXT
     );
     """)
 
@@ -19,8 +28,16 @@ def setup_database():
     cur.execute("""
     CREATE TABLE IF NOT EXISTS Ingredients (
         id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE
+    );
+    """)
+
+    #specific ingredient relationships
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Recipe_Ingredients (
         recipe_id INTEGER,
-        ingredient_name TEXT
+        ingredient_id INTEGER,
+        UNIQUE(recipe_id, ingredient_id)
     );
     """)
 
@@ -30,20 +47,29 @@ def setup_database():
         id INTEGER PRIMARY KEY,
         place_id TEXT UNIQUE,
         name TEXT,
-        city TEXT,
-        cuisine TEXT,
+        city_id INTEGER,
+        cuisine_id INTEGER,
         latitude REAL,
         longitude REAL,
         distance REAL
     );
     """)
 
-    # weather
+    # weather - might remove last line
     cur.execute("""
     CREATE TABLE IF NOT EXISTS Weather (
         id INTEGER PRIMARY KEY,
-        city TEXT,
+        city_id INTEGER UNIQUE,
         main_group TEXT
+        weather_des TEXT 
+    );
+    """)
+
+    #cities
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Cities (
+        id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE 
     );
     """)
 
