8d1ef640cd32d5aa2fc4d1c329ea9ac0b24c3917
Kerrington Jackson <kerryjac@umich.edu>
2025-12-14 05:54:07 -0500

    worked on geoapify places function/tables

--- geoapify.py
+++ geoapify.py
@@ -1,24 +1,52 @@
 import requests
 import sqlite3
-import json
+from api_key import geoapify_api_places
+
+limit = 25
+
+def offset_restaurants(): #pagination #avoid duplicates
+    conn = sqlite3.connect("project.db")
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM restaurants")
+    offset = cur.fetchone()[0]
+    conn.close()
+    return offset
 
 
+conn = sqlite3.connect("project.db")
+cur = conn.cursor()
 
-def restaurants_places(api_key, long, lat, cuisine):
+cur.execute("SELECT id, name FROM cuisines")
+cuisines = cur.fetchall()
 
+for cuisine_id, cuisine_name in cuisines:
     base = 'https://api.geoapify.com/v2/places?'
     params = {
-        'apiKey': api_key,
-        'categories': 'catering.restaurant',
-        'filter': f'circle:{long}, {lat}, 5000',
-        'bias': f'proximity: {long}, {lat}',
+        "categories": "catering.restaurant",
+        "filter": "circle:-83.7430,42.2808,10000", #ann arbor latitude and longitude
+        "offset": offset_restaurants(),
+        "apiKey": geoapify_api_places,
+        "limit": limit
     }
-
     response = requests.get(base, params=params)
     info = response.json()
 
-    features = info.get('features', [])
+    places = info.get('features', [])
 
-    #need to add properties code
-    #make sure open weather code stores long and lat values
-    #to use here
+    for place in places:
+        properties = place["properties"]
+        city = properties.get("city", "Unknown")
+
+        cur.execute("INSERT OR IGNORE INTO cities (name) VALUES (?)", (city,))
+        cur.execute("SELECT id FROM cities WHERE name=?", (city,))
+        city_id = cur.fetchone()[0]
+
+        cur.execute("""
+        INSERT OR IGNORE INTO restaurants
+        (geoapify_id, name, city_id, cuisine_id, latitude, longitude, distance)
+        VALUES (?, ?, ?, ?, ?, ?, ?)""", (properties["place_id"], properties.get("name", "Unknown"),
+        city_id, cuisine_id, properties["lat"], properties["lon"], properties.get("distance", 0)))
+
+conn.commit()
+conn.close()
+print("Data from Geoapify has been stored.")
