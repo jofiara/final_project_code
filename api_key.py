06d6492d55f96e57aceca5698e1b76e28a061014
Kerrington Jackson <kerryjac@umich.edu>
2025-12-14 11:22:50 -0500

    updated key again

--- api_key.py
+++ api_key.py
@@ -1,4 +1,4 @@
 open_weather_api = "34f81e8e455562918d5fbfdb25ca4b5c"
-spoonacular_api = "2b87c44a64d54af8932aa53679d126e"
+spoonacular_api = "536c12ac1684424aac02583dcee92438"
 geoapify_api_places = "bd4b577a11da4f76bc0ccaf9700fd801"
 geoapify_api_details = "328a81ac7a2f449a9833fc2aa5fa75d7"
