76df3b8d1001901b20489be2995611c388044ecc
Kerrington Jackson <kerryjac@umich.edu>
2025-12-15 17:30:48 -0700

    changes

--- open_weather.py
+++ open_weather.py
@@ -8,19 +8,18 @@ conn = sqlite3.connect("project.db")
 cur = conn.cursor()
 
 cur.execute("""
-SELECT DISTINCT c.name
+SELECT DISTINCT c.id, c.name
 FROM restaurants r
-JOIN cities c ON r.city_id = c.id""")
-rest_cities = [row[0] for row in cur.fetchall()]
+JOIN cities c ON r.city_id = c.id
+LEFT JOIN weather w ON c.id = w.city_id
+WHERE w.city_id IS NULL
+ORDER BY c.id
+LIMIT ?
+""", (limit,))
 
-cities_list = []
-for city in rest_cities:
-    if city not in cities_list:
-        cities_list.append(city)
-    if len(cities_list)>= limit:
-        break
+cities_list = cur.fetchall()
 
-for city_name in cities_list:
+for city_id, city_name in cities_list:
     base = "https://api.openweathermap.org/data/2.5/weather"
     params = {
             "appid": open_weather_api,
@@ -33,9 +32,6 @@ for city_name in cities_list:
     if "weather" not in info:
         continue
 
-    cur.execute("INSERT OR IGNORE INTO cities (name) VALUES (?)", (city_name,))
-    cur.execute("SELECT id FROM cities WHERE name=?", (city_name,))
-    city_id = cur.fetchone()[0]
 
     cur.execute("""
     INSERT OR IGNORE INTO weather
@@ -43,7 +39,7 @@ for city_name in cities_list:
     VALUES (?, ?, ?)""", (city_id, info["weather"][0]["main"], info["weather"][0]["description"]))
 
 conn.commit()
-cur.execute("SELECT COUNT(*) FROM cities")
-print(cur.fetchone()[0])
+cur.execute("SELECT COUNT(*) FROM weather")
+print(f"{cur.fetchone()[0]} cities' weather information is in the database.")
 conn.close()
 print("Data from OpenWeather has been stored.")
