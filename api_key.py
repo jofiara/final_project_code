23973dcce883434cd85222d9e328ac700e6a09dd
Kerrington Jackson <kerryjac@umich.edu>
2025-12-15 20:24:45 -0700

    deleted unnecessary key

--- api_key.py
+++ api_key.py
@@ -1,4 +1,3 @@
 open_weather_api = "34f81e8e455562918d5fbfdb25ca4b5c"
 spoonacular_api = "536c12ac1684424aac02583dcee92438"
 geoapify_api_places = "951773f5041049aaba3191b87be26ba8"
-geoapify_api_details = "328a81ac7a2f449a9833fc2aa5fa75d7"
